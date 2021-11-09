6fc827044bc633d7de7edc313108fa6b6dcc2db1
Maeda <93467607+fuzetsu4520@users.noreply.github.com>
2021-11-09 15:48:26 +0900

    test

---
+++ DamageCal/DamageCal/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DamageCal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 1:
+                    textBox1.Text = "90";
+                    textBox2.Text = "12";
+                    textBox3.Text = "20";
+                    break;
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
